69690d1166a11889fb9f592e23d159ecdd33808e
2382026 <2382026@unai.edu>
2024-09-03 10:24:26 +0700

    Handson 1

---
+++ TipeDataArray.java
@@ -0,0 +1,14 @@
+public class TipeDataArray {
+    public static void main(String[] args){
+        // Array adalah kumpulan dari sebuah data dengan tipe data dengan tipe data yang sama
+        //Array one dimension
+        int[] kumpulanAngka = new int [3];
+        kumpulanAngka[0] = 10;
+        kumpulanAngka[1] = 40;
+        kumpulanAngka[2] = 50;
+        System.out.println(kumpulanAngka[2]);
+
+        //Array multi-dimension
+        int[][] kumpulanAngka
+    }
+}
---
+++ TipeDataCharacterandBoolean.java
@@ -0,0 +1,2 @@
+public class TipeDataCharacterandBoolean {
+}
